6e85561691dcd6fd27e8076ed962289bea587eea
lurgi <wahoo9040@naver.com>
2023-04-07 12:55:01 +0900

    Updates

---
+++ _next/static/CuyPeNi9FjUkEFBF1P-pV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/734-4966c24fc92c4a79.js","static/chunks/pages/index-1521c628bda463b0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
